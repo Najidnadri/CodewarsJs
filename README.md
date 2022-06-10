e4144b34b91bb6fa39da0ff489999e298122413d
Najidnadri <muhd.najid.nadri@gmail.com>
2022-06-10 15:53:36 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# My CodeWars Solutions (JavaScript)
+
+A place where I put all my codewars solutions here as my future references
+(or others).
---
+++ practice.js
@@ -0,0 +1,104 @@
+//Below are all the personal practice I have done in codewars website
+
+
+//--------------------------------
+//-------------KYU 8--------------
+//--------------------------------
+
+//question link: https://www.codewars.com/kata/57e76bc428d6fbc2d500036d
+function stringToArray(string){
+    return string.split(' ');
+}
+
+//question link: https://www.codewars.com/kata/57eae20f5500ad98e50002c5
+function noSpace(x){
+    return x.replace(/ /g,'')
+}
+
+
+//question link: https://www.codewars.com/kata/596c6eb85b0f515834000049
+var replaceDots = function(str) {
+    let string_array = str.split(".");
+    return string_array.join("-");
+}
+
+
+//question link: https://www.codewars.com/kata/5302d846be2a9189af0001e4
+function sayHello(name, city, state) {
+    var fullName = name.join(" ");
+    return "Hello, " + fullName + "! Welcome to " + city + ", " + state + "!";
+}
+
+
+
+
+//--------------------------------
+//-------------KYU 7--------------
+//--------------------------------
+
+//question link: https://www.codewars.com/kata/5a523566b3bfa84c2e00010b/javascript
+function minSum(arr) {
+    //strategy: largest digit multiply with lowest digit
+      
+    //sort the array first
+    let sortedArr = arr.sort(function(a, b){return a - b});
+  
+    //split half
+    let halfIndex = sortedArr.length / 2;
+    let firstHalf = sortedArr.slice(0, halfIndex);
+    let secondHalf = sortedArr.slice(-halfIndex);
+    secondHalf.reverse()
+    
+    //compute
+    let totalArr = [];
+    for (let i = 0; i < firstHalf.length; i++) {
+      totalArr.push(firstHalf[i] * secondHalf[i]);
+    }
+    let total = totalArr.reduce((a, b) => a + b, 0);
+    return total
+}
+
+//question link: https://www.codewars.com/kata/592e830e043b99888600002d
+const alphabets = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+function encode(str,  n)
+{
+  //first step encoding
+  var chars = str.split("");
+  var encode = [];
+  for (let i = 0; i < chars.length; i++) {
+      let index = alphabets.indexOf(chars[i]) + 1;
+      encode.push(index);
+  }
+  console.log(encode);
+  
+  //second step encoding
+  let myFunc = num => Number(num);
+  var intArr = Array.from(String(n), myFunc);
+  var intIndex = 0;
+  
+  for (let i = 0; i < encode.length; i++) {
+      if (intIndex === intArr.length) {
+          intIndex = 0;
+      }
+      encode[i] += intArr[intIndex];
+      intIndex += 1;
+  }
+  console.log(encode);
+  return encode;
+}
+
+
+//--------------------------------
+//-------------KYU 6--------------
+//--------------------------------
+
+
+//--------------------------------
+//-------------KYU 5--------------
+//--------------------------------
+
+
+//--------------------------------
+//-------------KYU 4--------------
+//--------------------------------
